61ca4e0e20b47633829cba652cb0bc2f7dd5aedf
enzoblain <blenzodu57@gmail.com>
2025-12-03 23:13:09 +0100

    ci: sha2 sequential implementation for benchmark

--- benches/sha256_ref.rs
+++ benches/sha256_ref.rs
@@ -3,13 +3,21 @@ use sha2::{Digest, Sha256};
 use std::hint::black_box;
 
 pub fn bench_sha2_ref(c: &mut Criterion) {
-    c.bench_function("sha256_ref", |b| {
+    let mut g = c.benchmark_group("sha256_ref");
+
+    g.sample_size(1);
+    g.warm_up_time(std::time::Duration::ZERO);
+    g.measurement_time(std::time::Duration::from_millis(50));
+
+    g.bench_function("sha256_ref", |b| {
         b.iter(|| {
             let mut hasher = Sha256::new();
             hasher.update(black_box(&[0u8; 64]));
             let _ = hasher.finalize();
         })
     });
+
+    g.finish();
 }
 
 criterion_group!(benches, bench_sha2_ref);
